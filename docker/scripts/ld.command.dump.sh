67e3929c078b24d8384cb9230dce2ea794c30d2b
Perttu Ehn <perttu.ehn@exove.fi>
2019-09-23 14:42:35 +0300

    Minor output notatation fixes (vars, better colors)

--- docker/scripts/ld.command.dump.sh
+++ docker/scripts/ld.command.dump.sh
@@ -11,16 +11,16 @@ function ld_command_dump_exec() {
         exit 1
     fi
 
-    echo "${Yellow}Using datestamp: $DATE${Color_Off}"
+    echo -e "${Yellow}Using datestamp: $DATE${Color_Off}"
     DUMPER="mysqldump --host "$CONTAINER_DB" -uroot -p"$MYSQL_ROOT_PASSWORD" --all-databases --lock-all-tables --compress --flush-logs --flush-privileges  --dump-date --tz-utc --verbose"
     docker-compose -f $DOCKER_COMPOSE_FILE exec $CONTAINER_DB sh -c "$DUMPER  2>/dev/null | gzip --fast -f > /var/db_dumps/db-container-dump-$DATE.sql.gz"
     cd $PROJECT_ROOT/$DATABASE_DUMP_STORAGE
     ln -sf db-container-dump-$DATE.sql.gz db-container-dump-LATEST.sql.gz
     cd $PROJECT_ROOT
-    echo "DB backup in db_dumps/db-container-dump-$DATE.sql.gz"
-    echo "DB backup symlink: db_dumps/db-container-dump-LATEST.sql.gz"
+    echo "DB backup in $DATABASE_DUMP_STORAGE/db-container-dump-$DATE.sql.gz"
+    echo "DB backup symlink: $DATABASE_DUMP_STORAGE/db-container-dump-LATEST.sql.gz"
 }
 
 function ld_command_dump_help() {
-    echo "Backup databases to db_dump -folder"
+    echo "Backup databases to $DATABASE_DUMP_STORAGE -folder"
 }

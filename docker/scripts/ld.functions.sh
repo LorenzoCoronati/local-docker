2091b5addcee8b7ef5fe48bd32728f8f456a5752
Perttu Ehn <perttu.ehn@exove.fi>
2019-09-23 12:06:04 +0300

    Make yml_move function require type and not trash files

--- docker/scripts/ld.functions.sh
+++ docker/scripts/ld.functions.sh
@@ -20,21 +20,30 @@ is_dockersync() {
 }
 
 # Copy conf of your choosing to project root, destroy leftovers.
+# Template must be found using filename patters
+# TPL-NAME => $DOCKER_YML_STORAGE/docker-compose.TPL-NAME.yml,
+# usually: docker/docker-compose.TPL-NAME.yml
 # Usage
-#   yml_move
-#   yml_move skeleton
+#   yml_move template-name
 yml_move() {
-    MODE=${1-'common'}
-    echo "MODE: $MODE"
+    MODE=$1
+    if [ -z "$MODE" ]; then
+       echo -e "${Red}Trying to use yml files without project type.${Color_Off}"
+       exit 1
+    fi
+
     if [ -f "$DOCKER_YML_STORAGE/docker-compose.$MODE.yml" ]; then
         echo "Using $DOCKER_YML_STORAGE/docker-compose.$MODE.yml as the docker-compose recipe."
-        mv -v $DOCKER_YML_STORAGE/docker-compose.$MODE.yml ./$DOCKER_COMPOSE_FILE
-        rm -f $DOCKER_YML_STORAGE/docker-compose.*.yml
+        cp $DOCKER_YML_STORAGE/docker-compose.$MODE.yml ./$DOCKER_COMPOSE_FILE
+    else
+        echo -e "${Red} Docker-compose template file missing: $DOCKER_YML_STORAGE/docker-compose.$MODE.yml"
     fi
-    if [ -f "$DOCKER_YML_STORAGE/docker-sync.$MODE.yml" ]; then
+    # NFS template does not have docker-sync -flavor, as it uses nfs mounts for folder sharing.
+    if [ "$MODE" != "nfs" ] && [ -f "$DOCKER_YML_STORAGE/docker-sync.$MODE.yml" ]; then
         echo "Using $DOCKER_YML_STORAGE/docker-sync.$MODE.yml as the docker-sync recipe."
-        mv -v $DOCKER_YML_STORAGE/docker-sync.$MODE.yml ./$DOCKERSYNC_FILE
-        rm -f $DOCKER_YML_STORAGE/docker-sync.*.yml
+        cp $DOCKER_YML_STORAGE/docker-sync.$MODE.yml ./$DOCKERSYNC_FILE
+    else
+        echo -e "${Red} Docker-sync template file missing: $DOCKER_YML_STORAGE/docker-sync.$MODE.yml"
     fi
 }
 

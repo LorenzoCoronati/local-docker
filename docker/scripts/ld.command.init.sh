624e7cd1abc54c3e0d95855d7e8a40d7f7caf901
Perttu Ehn <perttu.ehn@exove.fi>
2019-09-23 12:08:09 +0300

    Make init ask more and store values to .env

--- docker/scripts/ld.command.init.sh
+++ docker/scripts/ld.command.init.sh
@@ -4,26 +4,85 @@
 # This file contains init -command for local-docker script ld.sh.
 
 function ld_command_init_exec() {
+
+    VALID=0
+    while [ "$VALID" -eq "0" ]; do
+        echo  -e "${BBlack}What is the project name?${Color_Off}"
+        echo  "Provide a string without spaces and use chars a-z and 0-9."
+        PROJECT_NAME=$(basename $PROJECT_ROOT)
+        read -p "Project name ['$PROJECT_NAME']: " ANSWER
+        if [ -z "$ANSWER" ]; then
+            VALID=1
+        elif [[ "$ANSWER" =~ ^[a-z0-9]([a-z0-9_-]*[a-z0-9])?$ ]]; then
+            PROJECT_NAME=$ANSWER
+            VALID=1
+        else
+            echo -e "${Red}ERROR: Project name can contain only alphabetic characters (a-z), numbers (0-9), underscore (_) and hyphen (-).${Color_Off}"
+            echo -e "${Red}ERROR: Also the project name must not start or end with underscore or hyphen.${Color_Off}"
+            sleep 2
+            echo
+        fi
+    done;
+    # Remove spaces.
+    PROJECT_NAME=$(echo "$PROJECT_NAME" | sed 's/[[:space:]]/-/g')
+
+    ensure_envvar_present PROJECT_NAME $PROJECT_NAME
+
+    echo  -e "${BBlack}What is the local development domain?${Color_Off}"
+    echo  " Do not add protocol, but just the domain name. For clarity it is recommended to use specified domains locally."
+    read -p "Domain [$PROJECT_NAME.ld] " LOCAL_DOMAIN
+    case "$LOCAL_DOMAIN" in
+        '') LOCAL_DOMAIN="$PROJECT_NAME.ld"
+    esac
+    # Remove spaces.
+    LOCAL_DOMAIN=$(echo "$LOCAL_DOMAIN" | sed 's/[[:space:]]/./g')
+    ensure_envvar_present LOCAL_DOMAIN $LOCAL_DOMAIN
+
+    echo  -e "${BBlack}What is the local development IP address?${Color_Off}"
+    echo  "Random 10.10.0.0./16 will be generated for you if you so wish?"
+    read -p "Use the random IP address 10.10.0.0./16 [Y/n]? " LOCAL_IP
+    case "$LOCAL_IP" in
+        'n'|'N'|'no'|'NO') LOCAL_IP='127.0.0.1';;
+        *) LOCAL_IP=$( printf "10.10.%d.%d\n" "$((RANDOM % 256))" "$((RANDOM % 256))");;
+    esac
+    echo -e "${Yellow}Using IP address $LOCAL_IP.${Color_Off}"
+    ensure_envvar_present LOCAL_IP $LOCAL_IP
+
+    # 2nd param, project type.
+    TYPE=${1-'common'}
     # Suggest Skeleton cleanup only when it is relevant.
-    APP_ROOT='app/'
-    if [ ! -e "$DOCKERSYNC_FILE" ] || [ ! -e "$DOCKER_COMPOSE_FILE" ]; then
-        echo "Copying Docker compose/sync files. What is project type? "
-        echo " [0] New project, application built in ./$APP_ROOT -folder [default]"
-        #echo " [1] Old project, application built in ./$APP_ROOT -folder "
-        echo " [2] Skeleton -proejct. Drupal in drupal/ and custom code spread in src/ folder."
-        read -p "Project type: " CHOICE
+    if [ -e "$DOCKERSYNC_FILE" ] || [ -e "$DOCKER_COMPOSE_FILE" ]; then
+        echo -e "${BYellow}WARNING: There is docker-compose and/or docker-sync recipies in project root.${Color_Off}"
+        echo -e "${BYellow}If you continue your containers with their volumes ${BRed}will be destroyed.${Color_Off}"
+        echo -e "However it this does not delete code from your application root directory."
+        read -p "Continue? [y/N]" CHOICE
         case "$CHOICE" in
-            ''|0|1 ) yml_move;;
-            2 ) APP_ROOT='drupal/'; yml_move skeleton;;
-            * ) echo "ERROR: Unclear answer, exiting" && cd $CWD && exit;;
-        esac
-        echo 'APP_ROOT='$APP_ROOT >> $PROJECT_ROOT/.env
-        read -p "Use project-name based docker-sync -volumes [default]? [Y/n]" CHOICE
-        case "$CHOICE" in
-            ''|y|Y|'yes'|'YES' ) $SCRIPT_NAME rename-volumes;;
-            n|N|'no'|'NO' ) echo "Volume names will start with 'webroot-'";;
+            n|N|'no'|'NO' ) echo "Cancelled." && exit;;
         esac
     fi
+
+    echo "Setting up docker-compose and docker-sync files for project type '$TYPE'."
+    yml_move $TYPE
+
+    # Skeleton uses different folder as the main location for app code.
+    [[ "$TYPE" == "skeleton" ]] &&  APP_ROOT='drupal'
+
+    APP_ROOT=${APP_ROOT:-app}
+    ensure_envvar_present APP_ROOT $APP_ROOT
+    ensure_folders_present $APP_ROOT
+    echo -e "${Yellow}NOTE: Application root is in $APP_ROOT.${Color_Off}"
+
+    DATABASE_DUMP_STORAGE=${DATABASE_DUMP_STORAGE:-db_dumps}
+    ensure_envvar_present DATABASE_DUMP_STORAGE $DATABASE_DUMP_STORAGE
+    ensure_folders_present $DATABASE_DUMP_STORAGE
+    echo -e "${Yellow}Database dumps will appear in $DATABASE_DUMP_STORAGE.${Color_Off}"
+
+    read -p "Use project-name based docker-sync -volumes [default]? [Y/n]" CHOICE
+    case "$CHOICE" in
+        ''|y|Y|'yes'|'YES' ) $SCRIPT_NAME rename-volumes $PROJECT_NAME;;
+        *) echo "Volume names will start with 'webroot-'";;
+    esac
+
     if [[ "$(docker-compose -f $DOCKER_COMPOSE_FILE ps)" ]]; then
         echo "Turning off current container stack."
         docker-compose -f $DOCKER_COMPOSE_FILE down

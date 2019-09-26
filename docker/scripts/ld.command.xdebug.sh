9f3e7d5a21dc3965e4e5c12151827cc2851c2dfd
Perttu Ehn <perttu.ehn@exove.fi>
2019-09-26 09:26:01 +0300

    Issue 53 Verify PHP container exists before Xdebug command executes

--- docker/scripts/ld.command.xdebug.sh
+++ docker/scripts/ld.command.xdebug.sh
@@ -4,6 +4,11 @@
 # This file contains stop -command for local-docker script ld.sh.
 
 function ld_command_xdebug_exec() {
+    if [ -z "$CONTAINER_PHP" ]; then
+        echo -e "${Red}ERROR: PHP container name is missing.${Color_Off}"
+        echo -e "${Red}Ensure you have variable 'CONTAINER_PHP' set in configuration.${Color_Off}"
+        exit 1
+    fi
     case "$1" in
         '1'|'on') ensure_envvar_present PHP_XDEBUG_REMOTE_ENABLE 1; sleep 1; docker-compose -f $DOCKER_COMPOSE_FILE up -d php && $SCRIPT_NAME xdebug;;
         '0'|'off') ensure_envvar_present PHP_XDEBUG_REMOTE_ENABLE 0; docker-compose -f $DOCKER_COMPOSE_FILE up -d php && $SCRIPT_NAME xdebug;;

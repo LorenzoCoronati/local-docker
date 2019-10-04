d2e9b59dd18d0f5f01b4c58981722f631309dbd1
Perttu Ehn <perttu.ehn@exove.fi>
2019-10-04 17:06:11 +0300

    Make self-update more robust against non-existing tags

--- docker/scripts/ld.command.self-update.sh
+++ docker/scripts/ld.command.self-update.sh
@@ -5,19 +5,34 @@
 
 function ld_command_self-update_exec() {
     TAG=${1}
+
     if [ -z "$TAG" ]; then
       echo -e "${Red}ERROR: You must provide a release tag to update to.${Color_Off}"
       return 1
     fi
-    mkdir ld-tmp
+
+    mkdir -v ld-tmp
+    curl -o ld-tmp/${TAG}.tar.gz https://codeload.github.com/Exove/local-docker/tar.gz/${TAG}
+    # Curl creates an ASCII file out of 404 response. Let's see what we have in the file.
+    INFO=$(file -b ld-tmp/${TAG}.tar.gz | cut -d' ' -f1)
+
+    if [ "$INFO" != "gzip" ]; then
+      echo -e "${Red}ERROR: Specifidd tag not found.${Color_Off}"
+      rm -rf ld-tmp
+      return 1
+    fi
+
     cd ld-tmp
-    curl -o ${TAG}.tar.gz https://codeload.github.com/Exove/local-docker/tar.gz/${TAG}
+    tar xvzf ${TAG}.tar.gz
     cp -r local-docker-${TAG}/docker ../
     cp -r local-docker-${TAG}/ld.sh ../
     cp -r local-docker-${TAG}/.env.example ../
     cp -r local-docker-${TAG}/.gitignore.example ../
     cd ..
     rm -rf ld-tmp
+    echo -e "${Green}Project updated to version ${BGreen}$TAG.${Color_Off}"
+    echo -e "${Green}Review and commit chages to ./docker, ld.sh, .env.example and .gitignore.example.${Color_Off}"
+    echo -e "${Green}Review updates to .env.example and optionally update yoru local .env file, too.${Color_Off}"
 }
 
 function ld_command_self-update_help() {

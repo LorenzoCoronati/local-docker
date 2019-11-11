51144ce673d869a6d957b7483346c5b201654dcd
Perttu Ehn <perttu.ehn@exove.fi>
2019-11-11 11:00:56 +0200

    Issue 81: Allow use of latest release and erify tag exists before use

--- docker/scripts/ld.command.self-update.sh
+++ docker/scripts/ld.command.self-update.sh
@@ -4,25 +4,30 @@
 # This file contains self-update -command for local-docker script ld.sh.
 
 function ld_command_self-update_exec() {
-    TAG=${1}
-
-    if [ -z "$TAG" ]; then
-      echo -e "${Red}ERROR: You must provide a release tag to update to.${Color_Off}"
-      return 1
+    TAG=${1:-latest}
+    # 'latest' is not a tag
+    if [ "$TAG" != "latest" ]; then
+      # GET /repos/:owner/:repo/releases/tags/:tag
+      EXISTS=$(curl -I https://api.github.com/repos/Exove/local-docker/releases/tags/${TAG} | head -1 |grep '200 OK' |wc -l)
+      if [ "$EXISTS" -eq "0" ]; then
+        echo -e "${Red}ERROR: Specifidd tag not found.${Color_Off}"
+        return 1;
+      fi
     fi
 
-    mkdir -v ld-tmp
-    curl -o ld-tmp/${TAG}.tar.gz https://codeload.github.com/Exove/local-docker/tar.gz/${TAG}
+    DIR=".ld-tmp-" . $(date +%s)
+    mkdir -v $DIR
+    curl -o $DIR/${TAG}.tar.gz https://codeload.github.com/Exove/local-docker/tar.gz/${TAG}
     # Curl creates an ASCII file out of 404 response. Let's see what we have in the file.
-    INFO=$(file -b ld-tmp/${TAG}.tar.gz | cut -d' ' -f1)
+    INFO=$(file -b $DIR/${TAG}.tar.gz | cut -d' ' -f1)
 
     if [ "$INFO" != "gzip" ]; then
       echo -e "${Red}ERROR: Specifidd tag not found.${Color_Off}"
-      rm -rf ld-tmp
+      rm -rf $DIR
       return 1
     fi
 
-    cd ld-tmp
+    cd $DIR
     tar xvzf ${TAG}.tar.gz
     cp -r local-docker-${TAG}/docker ../
     cp -r local-docker-${TAG}/ld.sh ../
@@ -30,11 +35,11 @@ function ld_command_self-update_exec() {
     cp -r local-docker-${TAG}/.gitignore.example ../
     cd ..
     rm -rf ld-tmp
-    echo -e "${Green}Project updated to version {BGreen}${TAG}${Green}.${Color_Off}"
+    echo -e "${Green}Project updated to version ${BGreen}${TAG}${Green}.${Color_Off}"
     echo -e "${Yellow}Review and commit changes to ./docker, ld.sh, .env.example and .gitignore.example.${Color_Off}"
     echo -e "${Yellow}Review updates in .env.example and optionally update your own .env file, too.${Color_Off}"
 }
 
 function ld_command_self-update_help() {
-    echo "Updates local-docker to a specified version."
+    echo "Updates local-docker to a specified version. Omitting version updates to the latest published full release for local-docker."
 }

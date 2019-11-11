e98d38b459668e2523e7d0edc389f5bddd47a7e4
Perttu Ehn <perttu.ehn@exove.fi>
2019-11-11 11:54:09 +0200

    Issue 81: Use tags for updates, default to the latest tag available
    Decided to not use *releases* as they are more rare where as almost all
    releases are done based on tags.

--- docker/scripts/ld.command.self-update.sh
+++ docker/scripts/ld.command.self-update.sh
@@ -7,9 +7,9 @@ function ld_command_self-update_exec() {
     TAG=${1}
     # CHeck the tag exists if one is provided.
     if [ ! -z "$TAG" ]; then
-      echo "checking tag: $TAG"
+      echo "Verifying tag ${TAG} exists, read https://api.github.com/repos/Exove/local-docker/tags"
       # GET /repos/:owner/:repo/releases/tags/:tag
-      EXISTS=$(curl -sI https://api.github.com/repos/Exove/local-docker/releases/tags/${TAG} | head -1 |grep '200 OK' |wc -l)
+      EXISTS=$(curl -s https://api.github.com/repos/Exove/local-docker/tags | grep ${TAG} |wc -l)
       if [ "$EXISTS" -eq "0" ]; then
         echo -e "${Red}ERROR: Specifidd tag not found.${Color_Off}"
         return 1;
@@ -19,11 +19,18 @@ function ld_command_self-update_exec() {
     DIR=".ld-tmp-"$(date +%s)
     mkdir $DIR
     if [ -z "$TAG" ]; then
-      # GET /repos/:owner/:repo/releases/latest
-      curl -so $DIR/latest.tar.gz https://api.github.com/repos/Exove/local-docker/releases/latest
+      TAG='latest'
+      # Latest git tags is the first one in the file.
+      URL=$(curl -s https://api.github.com/repos/Exove/local-docker/tags | grep -A4 '"name"' | grep 'tarball_url' | head -1)
+      URL=$(echo $URL |cut -d'"' -f4)
+      # -L to follow redirects
+      curl -L -s -o $DIR/${TAG}.tar.gz $URL
+
     else
-      # GET /repos/:owner/:repo/releases/:release_id
-      curl -so $DIR/${TAG}.tar.gz https://api.github.com/repos/Exove/local-docker/tar.gz/${TAG}
+      URL="https://api.github.com/repos/Exove/local-docker/tarball/${TAG}"
+      # -L to follow redirects
+      curl -L -s -o $DIR/${TAG}.tar.gz $URL
+      ls -lah $DIR
     fi
 
     # Curl creates an ASCII file out of 404 response. Let's see what we have in the file.
@@ -42,12 +49,12 @@ function ld_command_self-update_exec() {
     cp -r local-docker-${TAG}/.env.example ../
     cp -r local-docker-${TAG}/.gitignore.example ../
     cd ..
-    rm -rf ld-tmp
+    rm -rf $DIR
     echo -e "${Green}Project updated to version ${BGreen}${TAG}${Green}.${Color_Off}"
     echo -e "${Yellow}Review and commit changes to ./docker, ld.sh, .env.example and .gitignore.example.${Color_Off}"
     echo -e "${Yellow}Review updates in .env.example and optionally update your own .env file, too.${Color_Off}"
 }
 
 function ld_command_self-update_help() {
-    echo "Updates local-docker to a specified version. Omitting version updates to the latest published full release for local-docker."
+    echo "Updates local-docker to a specified git tag, see https://github.com/Exove/local-docker/tags. Defaults to the latest tag for local-docker."
 }

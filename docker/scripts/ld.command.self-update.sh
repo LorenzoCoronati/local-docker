6ed28c87bdc5edb5f77cd2637f2607f999249657
Perttu Ehn <perttu.ehn@exove.fi>
2019-12-02 11:02:00 +0200

    Update self-update -command info (even if not used)

--- docker/scripts/ld.command.self-update.sh
+++ docker/scripts/ld.command.self-update.sh
@@ -6,9 +6,10 @@
 function ld_command_self-update_exec() {
     echo -e "${BYellow}This command is removed.${Color_Off}"
     echo -e "${Yellow}You can update the local-docker issuing command from your PROJECT_ROOT.${Color_Off}"
-    echo -e "${Yellow}docker/scripts/self-update.sh TAG.${Color_Off}"
+    echo -e "\$ ${Yellow}docker/scripts/self-update.sh RELEASE-NAME.${Color_Off}"
+    echo -e "${Yellow}View available releases: ${BYellow}https://github.com/Exove/local-docker/releases${Yellow}.${Color_Off}"
 }
 
 function ld_command_self-update_help() {
-    echo "[REMOVED] Update local-docker with '\$ docker/scripts/self-update.sh TAG'."
+    echo "Updates local-docker to a specified release, see https://github.com/Exove/local-docker/releases. Defaults to the latest release."
 }

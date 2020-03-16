9784e5a254216a404a7d8b591399cedc2961aa1b
Perttu Ehn <perttu.ehn@exove.fi>
2020-03-16 13:57:52 +0200

    Add TLS/SSL cert inspector command

---
+++ docker/scripts/ld.command.tls-cert-check.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# File
+#
+# This file contains tls-cert-check -command for local-docker script ld.sh.
+
+function ld_command_tls-cert-check_exec() {
+    CERT_FOLDER="./docker/certs"
+    mkdir -p ${CERT_FOLDER}
+
+    CERTFILENAME="${PROJECT_NAME}--self-signed"
+    CERTFILEBASE_FULL="${CERT_FOLDER}/${CERTFILENAME}"
+
+
+    [ "$LD_VERBOSE" -ge "2" ] && echo -e "${BYellow}INFO:${Yellow} Checking certificate in: ${CERTFILEBASE_FULL}.crt${Color_Off}"
+    openssl x509 -in ${CERTFILEBASE_FULL}.crt -noout -text
+
+}
+
+function ld_command_tls-cert-check_help() {
+    echo "Check the local TLS certificate (HTTPS)."
+}
+
+
+function ld_command_tls-cert-check_extended_help() {
+    echo "Check the local TLS certificate (HTTPS)."
+    echo " "
+    echo "You can re-generate the certificate at any point."
+    echo "$ ./ld tls-cert"
+    echo " "
+}
